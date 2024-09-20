8c17f5bbf9b0088ffeb6419a8b19f8d074d42cc3
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-09-20 14:58:26 -0600

    Update and rename calculadora.C to calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,29 @@
+/* Edgar Eduardo Lorenzo Cruz 
+Ejercicio 1. practica 6
+  Hcer un algoritmo que perita sumar, restar, dividir y multplicar
+segun la eleccion del usuario.*/ 
+
+
+#include <stadio.h>
+#include <main.h>
+
+
+int main ()
+{
+  int a =0; b=0; operation=0;
+int option;
+
+printf("Dime que operacion deseas realizar: \n") ;
+printf("1 .- Suma\n") ; 
+printf("2  .- Resta\n") ;
+printf("3  .- Multiplicacion\n") ;
+printf("4 .- Division\n") ;
+printf("%d ,& option);
+
+  
+  
+  do
+  {
+
+
+
