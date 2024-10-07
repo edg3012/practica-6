02bccf106a51fc8c4179848cb0a8f4f4a057cdc6
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-10-06 22:19:38 -0600

    calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -4,26 +4,60 @@ Ejercicio 1. practica 6
 segun la eleccion del usuario.*/ 
 
 
-#include <stadio.h>
-#include <main.h>
+#include <stdio.h>
+#include <math.h>
 
 
 int main ()
 {
-  int a =0; b=0; operation=0;
-int option;
+    int a = 0, b = 0, operation = 0;
+    int option;
 
-printf("Dime que operacion deseas realizar: \n") ;
-printf("1 .- Suma\n") ; 
-printf("2  .- Resta\n") ;
-printf("3  .- Multiplicacion\n") ;
-printf("4 .- Division\n") ;
-printf("%d ,& option);
+    printf("Dime que operacion deseas realizar: \n");
+    printf("1 .- Suma\n");
+    printf("2 .- Resta\n");
+    printf("3 .- Multiplicacion\n");
+    printf("4 .- Division\n");
+    scanf("%d", &option);
 
-  
-  
-  do
-  {
+    
 
+    do
+    {
+        printf("Dame los numeros:\n");
+        printf("numero 1: ");
+        scanf("%d", &a);
+        printf("numero 2: ");
+        scanf("%d", &b);
 
+        switch (option)
+        {
+            case 1:
+                operation = a + b;
+                break;
+            case 2:
+                operation = a - b;
+                break;
+            case 3:
+                operation = a * b;
+                break;
+            case 4:
+                operation = a / b;
+                break;
+        }
 
+        printf("El resultado de la operacion es: %d\n", operation);
+
+        printf("Deseas realizar otra operacion: \n");
+        printf("1 .- Suma\n");
+        printf("2 .- Resta\n");
+        printf("3 .- Multiplicacion\n");
+        printf("4 .- Division\n");
+        printf("deseas salir: \n");
+        printf("-1 .- Salir\n");
+        scanf("%d", &option);
+    }
+    while (option != -1);
+
+    return 0;
+}
